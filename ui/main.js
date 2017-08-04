45e1ab18cde9f0db30efafb227cd6e4f543a8a6f
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-04 22:23:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,7 +14,7 @@ img.onclick=function(){
 
 var button=document.getElementById('counter');
 button.onclick = function(){
-    counter=counter + 1;
+    counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
 };
