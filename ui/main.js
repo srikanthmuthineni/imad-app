e7ae1c2bedbdebacd3afa0f0585dcaca1dfe9798
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-04 22:30:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,20 @@
-console.log('Loaded!');
-var counter=0;
-var element=document.getElementById('main-text');
-element.innerHTML="new value";
-var img=document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft= marginLeft + 'px';
-}
-img.onclick=function(){
-    var interval = setInterval(moveRight,50);
-};
+//console.log('Loaded!');
+//var element=document.getElementById('main-text');
+//element.innerHTML="new value";
+//var img=document.getElementById('madi');
+//var marginLeft = 0;
+//function moveRight(){
+   // marginLeft = marginLeft + 5;
+    //img.style.marginLeft= marginLeft + 'px';
+//}
+//img.onclick=function(){
+    //var interval = setInterval(moveRight,50);
+//};
 
 var button=document.getElementById('counter');
-button.onclick = function(){
+var counte = 0;
+button.onclick=function(){
     counter = counter + 1;
     var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    span.innerHTML=counter.toString();
 };
