856266188080ffd87eb4939212b787cc79d2e620
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-04 20:23:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="new value";
 var img=document.getElementById('madi');
-img.onClick=function(){
-    img.style.marginLeft='100px'
+img.onclick=function(){
+    img.style.marginLeft = '100px';
 }
