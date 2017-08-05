a6e270249e31b89a1b2d04c3e28907c8b477d3ca
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-05 11:22:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,12 +91,7 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
-app.get('/article-two', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
