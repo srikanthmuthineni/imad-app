ed3531d4f654dde1e230aac323f252e488bbc2f4
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-05 10:06:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,8 +13,21 @@ console.log('Loaded!');
 
 var button=document.getElementById('counter');
 var counter = 0;
-button.onclick=function(){
-    counter = counter + 1;
-    var span = document.getElementById('count');
+button.onclick=function()
+{
+    var request =new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.state === 200)
+            {
+                var counter=request.responseText;
+                 var span = document.getElementById('count');
     span.innerHTML=counter.toString();
+            }
+        }
+        
+    };
+    
+   
 };
