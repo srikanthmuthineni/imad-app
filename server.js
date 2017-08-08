7b632ae9c7c16a2938c91cdfb021576f70b78478
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-08 15:39:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var names = [];
-app.get('/submit-name/:name', function (req, res) {
+app.get('/submit-name', function (req, res) {
     
 var name = request.params.name;
 names.push(name);
