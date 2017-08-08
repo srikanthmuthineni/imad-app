12e7b960a2fd50a8a59c1106911e4bf1cec8e1a9
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-08 14:51:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,17 @@ console.log('Loaded!');
 var counter = 0;
 var button = document.getElementById("counter");
 button.onclick = function(){
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE);
+      {
+          if(request.status === 200)
+          {
+              var counter = request.responseText;
+              var span = document.getElementById("count");
+              span.innerHTML = count.toString();
+          }
+      }
+    };
+    request.open('GET','http://srikanthmuthineni@sh.imad.hasura-app.io/counter',true);
 };
