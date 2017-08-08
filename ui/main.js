bfbbf8ad6fd1a6831593b470bc7be797070dc7f9
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-08 15:12:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ button.onclick = function(){
           {
               var counter = request.responseText;
               var span = document.getElementById('count');
-              span.innerHTML = count.toString();
+              span.innerHTML = counter.toString();
           }
       }
     };
