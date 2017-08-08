f2f234e3e956af9a637753c663f6d3eb08327eed
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-08 15:08:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,6 +14,6 @@ button.onclick = function(){
           }
       }
     };
-    request.open('GET','http://srikanthmuthineni78@ssh.imad.hasura-app.io/counter/',true);
+    request.open('GET','http://srikanthmuthineni78.imad.hasura-app.io/counter/',true);
     request.send(null);
 };
