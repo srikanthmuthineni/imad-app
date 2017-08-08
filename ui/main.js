8c2970b208f7ba64229a62657c114c62ae34e0b6
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-08 17:46:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var button = document.getElementById("counter");
 button.onclick = function(){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
-      if(request.readyState === XMLHttpRequest.DONE);
+      if(request.readyState === XMLHttpRequest.DONE)
       {
           if(request.status === 200)
           {
@@ -17,17 +17,33 @@ button.onclick = function(){
     request.open('GET','http://srikanthmuthineni78.imad.hasura-app.io/counter',true);
     request.send(null);
 };*/
-var nameInput = document.getElementById('name');
-var value = nameInput.value;
+
+
 var submit = document.getElementById('submit-btn');
 submit.onclick = function(){
-    var name=['name1','name2','name3','name4'];
-    var list="";
-    for(var i=0;i<name.length;i++)
-    {
-        list += '<li>' + name[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+              if(request.status === 200)
+              {
+                  var names = request.responseText;
+                  names = JSON.parse(names);
+                  var list = "";
+                  for(var i=0;i<names.length;i++)
+                  {
+                      list += '<li>' + names[i] + '</li>';
+                  }
+                  var ul = document.getElementById('namelist');
+                  ul.innerHTML=list;
+                  
+              }
+        }
+        
+    };
+    var nameInput = document.getElementById('name');
+var value = nameInput.value;
+    request.open('GET','http://srikanthmuthineni78.imad.hasura-app.io/sumbit-name?name='+value,true);
+    request.send(null)
 };
 
