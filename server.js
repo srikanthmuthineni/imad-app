cbabaa503b114805573a6be8ac666c3051a23037
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-11 10:58:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var pool = require('pg').pool;
+var config={
+    user
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
