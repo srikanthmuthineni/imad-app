1954bfefbf5603fd8513bd9878133f10542ca32d
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-18 15:20:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,20 @@ var config = {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var pool = new pool(config);
+app.get('/test-db',function(req,res){
+   pool.query('select * from test',function(err,result){
+       
+      if(err){
+          res.status(500).send(err.toString());
+      } 
+      else{
+          
+          res.send(JSON.stringify(result.rows));
+      }
+   }); 
+    
+});
 
 var counter = 0;
 app.get('/counter', function (req, res) {
