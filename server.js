d9dced9dda111f16d5bc2e707ecdadfb37587adb
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-18 15:58:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config = {
     database:'srikanthmuthineni78',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:'process.env.DB-PASSWORD'
+    password:'process.env.DB_PASSWORD'
     
 };
 
