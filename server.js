366e7124fb7b961a2e9e5e0e05f5061f3255f95c
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-18 15:34:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var app = express();
-var pool = require('pg').Pool;
+var Pool = require('pg').pool;
 app.use(morgan('combined'));
 var config = {
     user:'srikanthmuthineni78',
