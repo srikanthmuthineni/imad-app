a9b50c36a6fe2de66f96ba20d2adf37a1c6e9ea0
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-19 10:09:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 
 function hash(input,salt)
 {
-    var hashed = crypto.pdkdf2Sync(input,salt,1000,512,'sha512');
+    var hashed = crypto.pbkdf2Sync(input,salt,1000,512,'sha512');
     return hashed.toString('hex');
     
 }
