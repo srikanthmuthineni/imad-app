c940c69639c4e1af71c47a119e2a8a39fb94b50e
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-20 15:56:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,6 +98,11 @@ app.get('/check-login',function(req,res){
    }
     
 });
+app.get('/logout',function(req,res){
+  delete req.session.auth;
+  res.send("logged out");
+    
+});
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
    pool.query('SELECT * FROM  test',function(err,result){
