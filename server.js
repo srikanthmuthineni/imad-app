8ef85d59817364c4a872063cc1f4cce4be47c7c1
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-21 20:12:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config = {
     port:'5432',
     password:process.env.DB_PASSWORD
 };
-var pool = new pool(config);
+var pool = new Pool(config);
 app.get('/test-db',function(req,res){
    pool.query('select * from test',function(err,result){
        if(err){
