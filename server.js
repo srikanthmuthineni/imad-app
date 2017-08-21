6a625442f2cde2eb2ad03394b176533ad337a1a2
srikanthmuthineni <30713456+srikanthmuthineni@users.noreply.github.com>
2017-08-21 11:19:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
 
 app.get('/articleone',function(req,res){
    
-   res.send('article one required and will be served here') ;
+    res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
 });
 app.get('/articletwo',function(req,res){
    
